6782bc861c7b124e1fb8a0c7ef6edf54bdaf7248
Breno Flaubert de Souza <brenoflaubert@gmail.com>
2024-07-09 13:13:49 -0300

    feat: trip controller post and get methods

---
+++ src/main/java/com/planner/planner/participant/ParticipantService.java
@@ -0,0 +1,15 @@
+package com.planner.planner.participant;
+
+import java.util.List;
+import java.util.UUID;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParticipantService {
+    public void registerParticipantsToEvent(List<String> participantsToInvite, UUID id){
+        return;
+    }
+    public void triggerConfirmationEmailToParticipants(UUID tripId){
+        return;
+    }
+}
